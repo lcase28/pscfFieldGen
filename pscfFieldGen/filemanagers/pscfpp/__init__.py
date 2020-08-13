e50e01d6592e898b09e083f4250be01511702815
Logan Case <case0234@umn.edu>
2020-08-13 14:44:33 -0500

    Added file managers for pscfpp and moved all file managers into submodule filemanagers.

---
+++ pscfFieldGen/filemanagers/pscfpp/__init__.py
@@ -0,0 +1,10 @@
+"""
+pscfpp subpackage
+
+A collection of classes to handle PSCF C++/Cuda file formats.
+"""
+
+# Collect relevant classes into one namespace
+
+from .paramfile import *
+from .commandscript import CommandScript
