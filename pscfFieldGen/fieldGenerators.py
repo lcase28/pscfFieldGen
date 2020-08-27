Corrected bug in numba-accellerated code (was missing 2pi factor in qnorm calculation). Removed pickle dump from __main__.py

pscfFieldGen/fieldGenerators.py
@@ -370,7 +370,7 @@ class UniformParticleField(FieldCalculatorBase):
         dim = len(brill[0,:])
         for i in range(nbrill):
             q = brill[i,:]
-            out[i] = np.sqrt( np.dot( q, np.dot( metTen, q ) ) )
+            out[i] = 2.0 * np.pi * np.sqrt( np.dot( q, np.dot( metTen, q ) ) )
         return out
     
     def to_kgrid(self, frac, ngrid,interfaceWidth=None, coreindex=0,lattice=None):